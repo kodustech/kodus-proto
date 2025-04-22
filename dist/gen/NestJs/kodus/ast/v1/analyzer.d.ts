932eb06d6dc8fde8ff8dbc27b1dcbf149ab00a35
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2025-04-22 13:07:11 -0300

    gitignore dist

---
+++ dist/gen/NestJs/kodus/ast/v1/analyzer.d.ts
@@ -0,0 +1,53 @@
+import { Observable } from "rxjs";
+export declare enum ProtoPlatformType {
+    PROTO_PLATFORM_TYPE_UNSPECIFIED = 0,
+    PROTO_PLATFORM_TYPE_GITHUB = 1,
+    PROTO_PLATFORM_TYPE_GITLAB = 2,
+    PROTO_PLATFORM_TYPE_JIRA = 3,
+    PROTO_PLATFORM_TYPE_SLACK = 4,
+    PROTO_PLATFORM_TYPE_NOTION = 5,
+    PROTO_PLATFORM_TYPE_MSTEAMS = 6,
+    PROTO_PLATFORM_TYPE_DISCORD = 7,
+    PROTO_PLATFORM_TYPE_AZURE_BOARDS = 8,
+    PROTO_PLATFORM_TYPE_AZURE_REPOS = 9,
+    PROTO_PLATFORM_TYPE_KODUS_WEB = 10,
+    PROTO_PLATFORM_TYPE_BITBUCKET = 11,
+    UNRECOGNIZED = -1
+}
+export declare enum ProtoAuthMode {
+    PROTO_AUTH_MODE_UNSPECIFIED = 0,
+    PROTO_AUTH_MODE_OAUTH = 1,
+    PROTO_AUTH_MODE_TOKEN = 2,
+    UNRECOGNIZED = -1
+}
+export interface BuildEnrichedGraphRequest {
+    baseRepo: RepositoryData | undefined;
+    headRepo: RepositoryData | undefined;
+}
+export interface BuildEnrichedGraphResponse {
+    data: string;
+    code: number;
+    error?: string | undefined;
+}
+export interface Auth {
+    type?: ProtoAuthMode | undefined;
+    token?: string | undefined;
+    org?: string | undefined;
+}
+export interface RepositoryData {
+    url: string;
+    provider: ProtoPlatformType;
+    branch?: string | undefined;
+    auth?: Auth | undefined;
+    organizationId: string;
+    repositoryId: string;
+    repositoryName: string;
+}
+export interface ASTAnalyzerServiceClient {
+    buildEnrichedGraph(request: BuildEnrichedGraphRequest): Observable<BuildEnrichedGraphResponse>;
+}
+export interface ASTAnalyzerServiceController {
+    buildEnrichedGraph(request: BuildEnrichedGraphRequest): Observable<BuildEnrichedGraphResponse>;
+}
+export declare function ASTAnalyzerServiceControllerMethods(): (constructor: Function) => void;
+export declare const AST_ANALYZER_SERVICE_NAME = "ASTAnalyzerService";
